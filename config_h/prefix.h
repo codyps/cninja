c1c4c510a38b7ce083063e6c3c4390add1fe066f
Cody P Schafer <dev@codyps.com>
2015-08-04 01:38:52 -0400

    config_h/prefix: add BYTE_ORDER probing
    Right now I'm trying very hard to avoid RUN tests, as they completely
    break cross compilation.
    
    For at least the versions of gcc and clang on this machine, these
    defines should allow us to completely determine the BYTE_ORDER, and on
    compilers that don't define the appropriate items, they should avoid
    giving us weird warnings.

--- config_h/prefix.h
+++ config_h/prefix.h
@@ -1,3 +1,18 @@
 /* This is a generated file. */
 #ifndef CONFIG_H_
 #define CONFIG_H_
+
+/* Probe byte-order via defines (clang & gcc at least work) to avoid run-time
+ * tests */
+#ifdef __BYTE_ORDER__
+# ifdef __ORDER_LITTLE_ENDIAN__
+#  if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#   define HAVE_LITTLE_ENDIAN 1
+#  endif
+# endif
+# ifdef __ORDER_BIG_ENDIAN__
+#  if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#   define HAVE_BIG_ENDIAN 1
+#  endif
+# endif
+#endif
